09ed7d42b888e2c620cf7c984bf294bd09fb36ae
Forrest Townsend <forrest.town18@gmail.com>
2013-11-22 16:18:24 -0700

    working on a directory to parse for games
    NOT FINISHED

--- ACMLauncher/GameLibrary/GameList.cs
+++ ACMLauncher/GameLibrary/GameList.cs
@@ -30,16 +30,20 @@ namespace ACMLauncher.GameLibrary
         public void FindAllGames()
         {
             //TODO: Decide to make a folder somewhere on the arcade cabinet and set the directory path equal to that
-            var dir = new DirectoryInfo(Path.GetFullPath(AppDomain.CurrentDomain.BaseDirectory));
-            dir = dir.Parent.Parent;
+            //var gameLibraryDir = new DirectoryInfo(Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory));
+
+            var folderList = new DirectoryInfo("C:\\GameLibrary").GetDirectories();
             //pathOfFolder.
 
-            foreach (var gameDirectory in dir.GetDirectories())
+            foreach (var gameDirectory in folderList)
             {
                 var game = new Game();
-                //game.PathToGameFolder = gameDirectory.FullName;
-                //game.GameName = //parse folder name OR executable
-                //game.Executable =
+                game.PathToGameFolder = gameDirectory.FullName;
+                game.GameName = gameDirectory.Name;
+                
+                //TODO: Fix this >>
+                //game.Executable = gameDirectory.GetFiles("*.exe");
+
                 //game.InializerFile =
                 //_gameList.Add(game);
             }
